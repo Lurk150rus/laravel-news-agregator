8db2a683bd4cbccea500b6a1304fe0279f0577de
Sasyk Kirill Vasilevich <sasyk@equip.me>
2026-05-22 15:25:56 +0300

    feat: add news model and migration

---
+++ database/migrations/2026_05_22_121841_create_news_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('news', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('external_id')->nullable();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->longText('content')->nullable();
+            $table->string('source');
+            $table->timestamp('received_at');
+            $table->unique('source', 'external_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('news');
+    }
+};
