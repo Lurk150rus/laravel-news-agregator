7327a0b23cbe2a98265fdeab1be557729edf5fec
Sasyk Kirill Vasilevich <sasyk@equip.me>
2026-05-30 20:47:12 +0300

    feat: enhance login form layout with Bootstrap styling and error handling

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,22 +2,68 @@
 
 @section('content')
 
-<h1>Login</h1>
+<div class="row justify-content-center">
+    <div class="col-md-5 col-lg-4">
 
-<form method="POST" action="/login">
-    @csrf
+        <div class="card shadow-sm border-0">
 
-    <input name="login" placeholder="login">
-    @error('login')
-        <div style="color:red;">{{ $message }}</>
-    @enderror
-    <input name="password" type="password" placeholder="password">
-    @error('password')
-        <div style="color:red;">{{ $message }}</>
-    @enderror
-    <button type="submit">Login</button>
-</form>
+            <div class="card-body p-4">
 
-<a href="/register">Register</a>
+                <h3 class="mb-4 text-center">Вход</h3>
+
+                <form method="POST" action="/login">
+                    @csrf
+
+                    {{-- LOGIN --}}
+                    <div class="mb-3">
+                        <label class="form-label">Логин</label>
+                        <input
+                            name="login"
+                            class="form-control @error('login') is-invalid @enderror"
+                            placeholder="Введите логин"
+                        >
+
+                        @error('login')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+
+                    {{-- PASSWORD --}}
+                    <div class="mb-3">
+                        <label class="form-label">Пароль</label>
+                        <input
+                            name="password"
+                            type="password"
+                            class="form-control @error('password') is-invalid @enderror"
+                            placeholder="Введите пароль"
+                        >
+
+                        @error('password')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+
+                    <button type="submit" class="btn btn-primary w-100">
+                        Войти
+                    </button>
+
+                </form>
+
+                <div class="text-center mt-3">
+                    <a href="/register" class="text-decoration-none">
+                        Нет аккаунта? Регистрация
+                    </a>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+</div>
 
 @endsection
