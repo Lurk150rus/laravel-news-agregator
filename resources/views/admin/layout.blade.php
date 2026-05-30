6bb00ca8778057aaec33dd6d57e80d8fd2556f9e
Sasyk Kirill Vasilevich <sasyk@equip.me>
2026-05-30 21:26:01 +0300

    feat: update admin layout with Bootstrap styling improvements and enhance navbar appearance

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -9,14 +9,17 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
-<body class="bg-light d-flex flex-column min-vh-100">
+<body class="bg-body-tertiary d-flex flex-column min-vh-100">
 
     {{-- NAVBAR --}}
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container-fluid">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary shadow-sm">
+            <div class="container-fluid">
 
             <a class="navbar-brand" href="{{ route('admin.dashboard') }}">
                 Admin Panel
+                <span class="badge bg-warning text-dark ms-2">
+                    ADMIN
+                </span>
             </a>
 
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#adminNav">
@@ -50,7 +53,7 @@
                 <ul class="navbar-nav ms-auto">
 
                     <li class="nav-item">
-                        <a class="nav-link text-warning" href="{{ route('news') }}">
+                        <a class="nav-link text-white" href="{{ route('news') }}">
                             ← Портал
                         </a>
                     </li>
@@ -64,10 +67,9 @@
 
     {{-- CONTENT --}}
     <div class="container py-4 flex-grow-1">
-
-
-        @yield('content')
-
+        <div class="bg-white rounded-3 shadow-sm p-4">
+            @yield('content')
+        </div>
     </div>
 
     {{-- NOTIFICATIONS --}}
